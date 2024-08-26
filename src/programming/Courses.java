02427eed4f675e840c353a473077dc24fef16826
Monias <khunwana.monias@gmail.com>
2024-08-26 18:07:40 +0200

    Using Strings in functional programming
    Signed-off-by: Monias <khunwana.monias@gmail.com>

---
+++ src/programming/Courses.java
@@ -0,0 +1,46 @@
+package programming;
+
+import java.util.List;
+
+public class Courses {
+	public static void main(String[] args) {
+		// ex1
+		printAllCoursesindividually(List.of("Spring","Spring boot","API","AWS","PCP","AZURE","DOCKER") ); //printing indidual string using for loop
+		//ex2
+		printAllCoursesindividuallyFunctional(List.of("Spring","Spring boot","API","AWS","PCP","AZURE","DOCKER") ); //printing indidual string using functiona programming
+		//ex3
+		printAllCoursesWithSpringindividuallyFunctional(List.of("Spring","Spring boot","API","AWS","PCP","AZURE","DOCKER") ); //using functional programming for String with Spring
+		//ex4
+		printAllCoursesWithAtleast4LettersindividuallyFunctional(List.of("Spring","Spring boot","API","AWS","PCP","AZURE","DOCKER") ); // using functional programming for String with more than 4 characters
+	}
+
+	private static void printAllCoursesindividually(List<String> courses) {
+		for (String course : courses )		
+		{
+			System.out.println(course);
+		}
+	}	
+	
+	private static void printAllCoursesindividuallyFunctional(List<String> courses) {
+		courses.stream()
+				.forEach(Courses::printCourses);
+	}	
+
+	private static void printAllCoursesWithSpringindividuallyFunctional(List<String> courses) {
+		courses.stream()
+				.filter(course -> course.contains("Spring"))
+				.forEach(Courses::printCourses);
+	}	
+	
+	private static void printAllCoursesWithAtleast4LettersindividuallyFunctional(List<String> courses) {
+		courses.stream()
+				.filter(course -> course.length() > 4)
+				.forEach(Courses::printCourses);
+	}	
+	
+	
+	private static void printCourses(String courses)
+	{
+		System.out.println(courses);
+	}
+}
